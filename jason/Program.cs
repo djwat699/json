f8061dc41adaa7894e8e775026e56305549055b7
danica <djwat699@learner.dvusd.org>
2022-03-22 09:28:35 -0700

    Added Project Files

---
+++ jason/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.Json;
+namespace jason
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var weatherForecast = new WeatherForecast
+            {
+                Date = DateTime.Parse("2019-08-01"),
+                TemperatureCelsius = 25,
+                Summary = "Hot"
+            };
+
+            var PhoenixForecast = new WeatherForecast
+            {
+                Date = DateTime.Parse("2022-03-22"),
+                TemperatureCelsius = 25,
+                Summary = "Hot"
+            };
+
+            string jsonString = JsonSerializer.Serialize(weatherForecast);
+
+            Console.WriteLine(jsonString);
+
+            string PheonixString = JsonSerializer.Serialize(weatherForecast);
+
+            Console.WriteLine(PheonixString);
+
+            WeatherForecast w = JsonSerializer.Deserialize<WeatherForecast>(PheonixString);
+
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
---
+++ jason/WeatherForecast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace jason
+{
+    class WeatherForecast
+    {
+        public DateTimeOffset Date { get; set; }
+        public int TemperatureCelsius { get; set; }
+        public string Summary { get; set; }
+    }
+}
